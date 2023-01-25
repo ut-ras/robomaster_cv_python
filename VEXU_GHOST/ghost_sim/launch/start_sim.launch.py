8c35f97e03795f0e3411a2c88cb8f18f2a2f7e8a
leo-witzke <leowitzke00@gmail.com>
2023-01-24 22:33:35 -0600

    merging in new stuff and some windows fixes

--- VEXU_GHOST/ghost_sim/launch/start_sim.launch.py
+++ VEXU_GHOST/ghost_sim/launch/start_sim.launch.py
@@ -25,9 +25,9 @@ def launch_setup(context, *args, **kwargs):
         enable_pid = True
 
     if(bool(enable_pid)):
-        filename = "ghost1_sim_pid.xacro"
+        filename = "ghost1_sim_pid.urdf"
     else:
-        filename = "ghost1_sim_voltage.xacro"
+        filename = "ghost1_sim_voltage.urdf"
 
     # Load URDF and process to text
     urdf_path = os.path.join(ghost_sim_share_dir, "urdf", filename)
@@ -58,6 +58,10 @@ def generate_launch_description():
     gazebo_ros_share_dir = get_package_share_directory('gazebo_ros')
     ghost_ros_share_dir = get_package_share_directory('ghost_ros')
     ghost_sim_share_dir = get_package_share_directory('ghost_sim')
+
+    home_dir = os.path.expanduser('~')
+    ghost_ros_base_dir = os.path.join(home_dir, "VEXU_GHOST", "ghost_ros")
+
     world_file = os.path.join(ghost_sim_share_dir, "urdf", "spin_up.world")
     rviz_config_path = os.path.join(ghost_ros_share_dir, 'rviz/urdf_config.rviz')
 
@@ -70,7 +74,14 @@ def generate_launch_description():
         launch_arguments={
             'world': world_file,
             'gui': LaunchConfiguration("sim_gui"),
+            'verbose': LaunchConfiguration('verbose'),
             }.items()
+    )
+
+    ground_truth_publisher = Node(
+        package='ghost_sim',
+        executable='ground_truth_pose_publisher',
+        name='ground_truth_pose_publisher',
     )
 
     # Launch RVIZ Display as primary GUI interface
@@ -94,20 +105,32 @@ def generate_launch_description():
         condition=launch.conditions.IfCondition(LaunchConfiguration("joystick"))
     )
 
-    ghost_ros_main = Node(
+    estimator_node = Node(
         package='ghost_ros',
-        executable='ghost_ros_main',
-        name='ghost_ros_main',
+        executable='ghost_estimator_node',
+        name='ghost_estimator_node',
         output='screen',
+        parameters=[ghost_ros_base_dir + "/config/ghost_estimator_config.yaml"]
+    )
+
+    state_machine_node = Node(
+        package='ghost_ros',
+        executable='robot_state_machine_node',
+        name='ghost_state_machine_node',
+        output='screen',
+        parameters=[ghost_ros_base_dir + "/config/ghost_state_machine_config.yaml"]
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(name='enable_pid', default_value='True'),
-        DeclareLaunchArgument(name='joystick', default_value='False'),
-        DeclareLaunchArgument('sim_gui', default_value='False'),
+        DeclareLaunchArgument(name='enable_pid', default_value='true'),
+        DeclareLaunchArgument(name='joystick', default_value='false'),
+        DeclareLaunchArgument('sim_gui', default_value='false'),
+        DeclareLaunchArgument('verbose', default_value='false'),
         simulation,
+        ground_truth_publisher,
         rviz_node,
         joy_launch_description,
-        ghost_ros_main,
+        estimator_node,
+        state_machine_node,
         OpaqueFunction(function = launch_setup)
     ])
