2a93c71236e0e2197460798e38664ea16230bea0
wenyuzhuut <70409369+wenyuzhuut@users.noreply.github.com>
2023-03-19 19:47:04 -0500

    New2

--- Abc/test.py
+++ Abc/test.py
@@ -2,36 +2,45 @@ from filterpy.kalman import KalmanFilter
 import matplotlib.pyplot as plt
 import numpy as np
 import array as arr
+import time
 from filterpy.common import Q_discrete_white_noise
-
-dt = 0.1
-ap = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-aq = np.array([20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1])
+timePast = 0
+#ap = np.array([0.0, 0.3, 1.2, 2.6999999999999997, 4.8, 7.5, 10.799999999999999, 14.7, 19.2, 24.3, 30.0, 36.3, 43.199999999999996, 50.699999999999996, 58.8, 67.5, 76.8, 86.7, 97.2, 108.3])
+#aq = np.array([0.0, 0.8, 3.2, 7.2, 12.8, 20.0, 28.8, 39.2, 51.2, 64.8, 80.0, 96.80000000000001, 115.2, 135.20000000000002, 156.8, 180.0, 204.8, 231.20000000000002, 259.2, 288.8])
+delt = 0.1
 f = KalmanFilter (dim_x=3, dim_z=1)
-g = KalmanFilter (dim_x=3, dim_z=1)
 f.x = np.array([100., 10., 1.])
-f.F = np.array([[1., dt, 0.5*(dt**2)],
-                [0., 1., dt],
-                [0., 0., 1.]])
 f.H = np.array([[1., 0., 0.]])
-f.P *= 1000.
 f.R = 5
-f.Q = Q_discrete_white_noise(dim=3, dt=0.1, var=0.13)
+
+g = KalmanFilter (dim_x=3, dim_z=1)
 g.x = np.array([100., 10., 1.])
-g.F = np.array([[1., dt, 0.5*(dt**2)],
-                [0., 1., dt],
-                [0., 0., 1.]])
 g.H = np.array([[1., 0., 0.]])
-g.P *= 1000.
 g.R = 5
-g.Q = Q_discrete_white_noise(dim=3, dt=0.1, var=0.13)
-i = 0
-while (i < 20):
-    x = ap[i]
-    y = aq[i]
-    f.predict()
-    g.predict()
-    print("(", x, ",", f.x[0], ")", " (", y, ",", g.x[0], ")")
+
+def kinematic_Predict(delt):
+    f.F = np.array([[1., delt, 0.5*(delt**2)],
+                    [0., 1., delt],
+                    [0., 0., 1.]])
+    f.P *= 1000.
+    f.Q = Q_discrete_white_noise(dim=3, dt=delt, var=0.13)
+    g.F = np.array([[1., delt, 0.5*(delt**2)],
+                    [0., 1., delt],
+                    [0., 0., 1.]])
+    g.P *= 1000.
+    g.Q = Q_discrete_white_noise(dim=3, dt=delt, var=0.13)
+    return np.array([f.predict(), g.predict()])
+
+def kinematic_Update(x, y):
     f.update(x)
     g.update(y)
-    i+=1
+# i = 0
+# while (i < 20):
+#     x = ap[i]
+#     y = aq[i]
+#     f.predict()
+#     g.predict()
+#     print("(", x, ",", f.x[0], ")", " (", y, ",", g.x[0], ")")
+#     f.update(x)
+#     g.update(y)
+#     i+=1
