671913ac9820ed4f84a264d9da6f1328721160d9
wenyuzhuut <70409369+wenyuzhuut@users.noreply.github.com>
2023-03-19 19:15:54 -0500

    New

--- Abc/test.py
+++ Abc/test.py
@@ -2,20 +2,36 @@ from filterpy.kalman import KalmanFilter
 import matplotlib.pyplot as plt
 import numpy as np
 import array as arr
-ap = arr.array('i', [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+from filterpy.common import Q_discrete_white_noise
+
+dt = 0.1
+ap = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+aq = np.array([20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1])
 f = KalmanFilter (dim_x=3, dim_z=1)
-f.x = np.array([100., 10.])
-f.F = np.array([[1.,1.],
-                [0.,1.]])
-f.H = np.array([[1.,0.]])
+g = KalmanFilter (dim_x=3, dim_z=1)
+f.x = np.array([100., 10., 1.])
+f.F = np.array([[1., dt, 0.5*(dt**2)],
+                [0., 1., dt],
+                [0., 0., 1.]])
+f.H = np.array([[1., 0., 0.]])
 f.P *= 1000.
 f.R = 5
-from filterpy.common import Q_discrete_white_noise
-f.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
+f.Q = Q_discrete_white_noise(dim=3, dt=0.1, var=0.13)
+g.x = np.array([100., 10., 1.])
+g.F = np.array([[1., dt, 0.5*(dt**2)],
+                [0., 1., dt],
+                [0., 0., 1.]])
+g.H = np.array([[1., 0., 0.]])
+g.P *= 1000.
+g.R = 5
+g.Q = Q_discrete_white_noise(dim=3, dt=0.1, var=0.13)
 i = 0
 while (i < 20):
-    z = ap[i]
+    x = ap[i]
+    y = aq[i]
     f.predict()
-    f.update(z)
-    print("(", ap[i], ",", f.x[0], ")")
+    g.predict()
+    print("(", x, ",", f.x[0], ")", " (", y, ",", g.x[0], ")")
+    f.update(x)
+    g.update(y)
     i+=1
