49e0872f17f2f6b5df09ba9e74643be6e93a9022
wenyuzhuut <70409369+wenyuzhuut@users.noreply.github.com>
2023-03-25 17:36:53 -0500

    Seperated Into Get and Predict Functions

--- Abc/test.py
+++ Abc/test.py
@@ -8,7 +8,7 @@ f.x = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.])
 f.H = np.array([[1., 0., 0., 0., 0., 0., 0., 0., 0.],
                 [0., 0., 0., 1., 0., 0., 0., 0., 0.],
                 [0., 0., 0., 0., 0., 0., 1., 0., 0.]])
-f.R = 5
+f.R = 5.
 def kinematic_predict(delt):
     f.F = np.array([[1.,    delt,   0.5*(delt**2),     0., 0., 0., 0., 0., 0.],
                 [0.,    1.,     delt,               0., 0., 0., 0., 0., 0.],
@@ -22,8 +22,14 @@ def kinematic_predict(delt):
     f.P *= 1000.
     f.Q = Q_discrete_white_noise(dim=3, dt=delt, block_size = 3, var=0.13, order_by_dim = True)
     f.predict()
-    #x, y, z, x', y', z', x'', y'', z''
-    return ([f.x[0], f.x[3], f.x[6], f.x[1], f.x[4], f.x[7], f.x[2], f.x[5], f.x[8]])
+    
+#x', y', z', x'', y'', z''
+def getVA():
+    return [f.x[1], f.x[4], f.x[7], f.x[2], f.x[5], f.x[8]]
+
+#x, y, z
+def getPredictedPos():
+    return[f.x[0], f.x[3], f.x[6]]
 
 def kinematic_Update(x, y, z):
     f.update([x,y,z])
