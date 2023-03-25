b5d21b4497839819a7cc2ddef8e081cc361eab6a
wenyuzhuut <70409369+wenyuzhuut@users.noreply.github.com>
2023-03-25 17:23:00 -0500

    New Ordereing for Prediciton Output

--- Abc/test.py
+++ Abc/test.py
@@ -3,12 +3,12 @@ import numpy as np
 from filterpy.common import Q_discrete_white_noise
 
 f = KalmanFilter (dim_x=9, dim_z=3)
-f.x = np.array([0., 0., 1., 0., 0., 1., 0., 0., 1.])
+#x, x', x'', y, y', y'', z, z', z''
+f.x = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.])
 f.H = np.array([[1., 0., 0., 0., 0., 0., 0., 0., 0.],
                 [0., 0., 0., 1., 0., 0., 0., 0., 0.],
                 [0., 0., 0., 0., 0., 0., 1., 0., 0.]])
 f.R = 5
-
 def kinematic_predict(delt):
     f.F = np.array([[1.,    delt,   0.5*(delt**2),     0., 0., 0., 0., 0., 0.],
                 [0.,    1.,     delt,               0., 0., 0., 0., 0., 0.],
@@ -22,7 +22,8 @@ def kinematic_predict(delt):
     f.P *= 1000.
     f.Q = Q_discrete_white_noise(dim=3, dt=delt, block_size = 3, var=0.13, order_by_dim = True)
     f.predict()
-    return ([f.x[0], f.x[3], f.x[6]])
+    #x, y, z, x', y', z', x'', y'', z''
+    return ([f.x[0], f.x[3], f.x[6], f.x[1], f.x[4], f.x[7], f.x[2], f.x[5], f.x[8]])
 
 def kinematic_Update(x, y, z):
     f.update([x,y,z])
