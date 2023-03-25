c7b295354d2cad5c10b1fa49379c00aa7b2ab659
Grace <flamewyrn15@gmail.com>
2023-03-25 16:16:34 -0500

    Started the test file for objectlog

---
+++ object_log/test.py
@@ -0,0 +1,39 @@
+from bounding_box import *
+#from armorplate import *
+#from objectlog import *
+import numpy as np
+
+
+num = 4
+
+def main():
+    boxlistPrev = []
+    boxlistNext = []
+    for i in range(num):
+        boxlistPrev[i] = BoundingBox()
+        boxlistPrev[i].set_x_value(np.random(0,150))
+        boxlistPrev[i].set_y_value(np.random(0,150))
+        boxlistPrev[i].set_depth_value(np.random(0,150))
+        boxlistPrev[i].set_height_value(np.random(0,150))
+        boxlistPrev[i].set_width_value(np.random(0,150))
+        boxlistPrev[i].set_time()
+
+    for i in range(num):
+        print(boxlistPrev[i].get_x_value())
+        print('\n')
+        print(boxlistPrev[i].get_y_value())
+        print('\n')
+        print(boxlistPrev[i].get_depth_value())
+        print('\n')
+        print(boxlistPrev[i].get_height_value())
+        print('\n')
+        print(boxlistPrev[i].get_width_value())
+        boxlistPrev[i].print_time()
+        print('\n')
+
+
+        
+
+    
+if __name__ == "__main__":
+    main()
