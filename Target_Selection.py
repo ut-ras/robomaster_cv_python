0bda9ca011e0dd55138a7c96db20dd1e1f66ecf4
wenyuzhuut <70409369+wenyuzhuut@users.noreply.github.com>
2023-04-04 17:38:17 -0500

    Comments

--- Target_Selection.py
+++ Target_Selection.py
@@ -1,13 +1,17 @@
 import numpy as np
 
+
 screenWidth = 1280
 screenHeight = 720
 mousePos = [screenWidth/2, screenHeight/2]
 #Assumes x, y as camera 
 class targetSelection(object):
-    def selectTarget(self, log, mousePos):
+    #Takes in an ObjectLog Object
+    #Returns the id of the Plate with the shortest distance
+    def selectTarget(self, log):
         shortest_dist = float('inf')
         shortest_plate = -1 # keeping the index, not decided what to do with the closest plate
+        #Loops through all armor plates and finds the closest plate by pythagorean theorem
         for i in range(len(log.plates)):
             if (np.pow((mousePos[0]-log.plates[i].boundingBox.__x_center__), 2)\
             +np.pow((mousePos[1]-log.plates[i].boundingBox.__y_center__), 2) < shortest_dist):
