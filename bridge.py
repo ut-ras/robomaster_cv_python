d1100ddcdde19bf7f6cc3118ed4096ae4375683a
HasifShaikh <hasifshaikh02@gmail.com>
2023-04-06 17:18:38 -0500

    Setup framework for the main loop and fixed some bad code. Will need to write a function in depth calc

--- bridge.py
+++ bridge.py
@@ -1,13 +1,32 @@
+import sys
 import pyrealsense2 as rs
 
 from depth_calculation import depth_calculation as dc
 from object_log import objectlog as ol
+import target_selection as ts
+import prediction as p
+import time
 
 def init():
+    #initialize ML model
     dc.initialize_real_sense()
     oLog = ol.objectlog()
 
 init()
 while True:
-    #oLog.boxesInput(Get_Bounding_box())
+    #oLog.update(Get_Bounding_box())
+    #grab ML bbo
+    temp = None
+    #feed bbo into depth calculation
+    
+    #feed temp into update
+    ol.boxesInput(temp, time.time())
+
+    #target select the object
+    sel = ts.selectTarget(ol)
+
+    #send to UART
+    #sendtoUART()
+
+    
     print("LOL")
