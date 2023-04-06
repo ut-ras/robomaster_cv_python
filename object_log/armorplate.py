1e7aff09dbd8d3f17dcfabbd75819bf4e1ceb051
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2023-04-05 23:59:26 -0500

    Object log compiles

--- object_log/armorplate.py
+++ object_log/armorplate.py
@@ -1,12 +1,11 @@
 import numpy as np
 import bounding_box as bb
-
+import prediction as pred
 
 
 class ArmorPlate:
     """
     A object of an armor plate object that is typically associated with what is seen.
-
     Object should be initialized from the outputs of the machine learning model and has parameters such as position, velocity, depth, etc.
     
     Armor plate object is used in object log
@@ -35,24 +34,23 @@ class ArmorPlate:
         self.lastTime = None
         self.history = None
         self.timestamp_history = None # Hasif told me to make this
-        self.assoc_plates = None 
+        self.assoc_plates = []
         self.max_assoc_plates = 5
+        self.kf = pred.Prediction()
 
     # velocity and acceleration are sets of three values
-    def updateVA(self, va_vector) -> int:
+    def updateVA(self):
+        va_vector = self.kf.getVA()
         self.velocity = va_vector[0:3]
         self.acceleration = va_vector[3:6]
-        return 0
 
     # use position, velocity, and delta time to predict where armor plate have moved to since the last check
     # this method is subject to change depending on how PVA is implemented (currently assumed to be world positions)
     # assumes that time is being kept track of in per second units while velocity/acceleration are per millisecond units
     def predictPosition(self, currentTime):
         delta_t = currentTime - self.lastTime
-        delta_t = delta_t
         self.nextPosition = self.position + (np.multiply(self.velocity, delta_t)) + (np.multiply(self.acceleration, np.exp(delta_t, 2) / 2)) # kinematics :D
-        self.lastTime = currentTime
-
+        
     def getPosition(self):
         return self.position 
 
@@ -73,16 +71,18 @@ class ArmorPlate:
     
     # given an armorplate we have associated in objectlog, 
     # add to a list of associated armor plates
-    def addArmorPlate(self, new_plate):
+    def addArmorPlate(self, new_plate, currentTime):
         #add to data structure
         if len(self.assoc_plates) == self.max_assoc_plates:
             self.assoc_plates.pop()
         self.assoc_plates.insert(0,new_plate)
+        self.kf.kinematicUpdate(new_plate.getPosition())
+        self.lastTime = currentTime
+
         return
-    
 
     def writeToHistory(self, historyFile):
         # historyFile = open("pathhere",'a')
-        historyFile.write(self.id + " " + self.position + " " + self.activity + " " )
-        historyFile.write(self.lastTime + ", ") # insert writing stuff here
+        historyFile.write("ID: {} Position: {} Activity: {} LastTime: {}\n"\
+                          .format(self.id, self.position, self.activity,self.lastTime))
         # historyFile.close()
--- object_log/objectlog.py
+++ object_log/objectlog.py
@@ -1,3 +1,8 @@
+from armorplate import *
+import numpy as np
+import bounding_box
+import filterpy
+
 #TODO figure out correct values for this
 #these are the constants used to calculate if a predicted position is out of bounds
 #these should be updated with the actual values
@@ -12,7 +17,11 @@ MIN_Z = -1
 MIN_AREA = 10
 
 #TODO margin of error used in 
-margin_of_err = 5 # this is some random number, we need to finetune this later
+margin_of_err = 0 # this is some random number, we need to finetune this later
+
+#TODO kill_threshold used for when we know something hasn't been seen for a 
+# while and needs to be kicked out
+kill_threshold = -1 # need to replace with an actual val
 
 """
 Object Log:
@@ -21,7 +30,6 @@ Fields:
 - plates: array of currently active armor plates (armor plate objects)
 - id: ID associated with them
 - Timestamp: The last timestamp they have.
-
 Primary functions in pipeline:
 - Take in input from depth/ML and associate armor plates
 - Filter out old plates
@@ -31,7 +39,6 @@ class objectlog:
 
     """
     Initialize function
-
     Runs in init() in main loop
     instantiates plates, ID var, and the timestamp variable. Opens output doc
     """
@@ -40,7 +47,7 @@ class objectlog:
         self.idAssign = 0
         
         #Python automatically creates the file with this name if it does not exist
-        self.objectLogOuput = open("ObjectLog.txt",'w')
+        self.objectLogOutput = open("ObjectLog.txt",'w')
 
 
     """
@@ -50,10 +57,7 @@ class objectlog:
     - timestamp: timestamp of the boundingBoxes
     the plates unless the closest distance is greater than some margin of error
     """
-    def boxesInput(self, boxList, timestamp, currentTime):
-
-        #self.timeStamp = timestamp #timestamp is used for finding the delta t to do the kinematics. This needs to change, it is wrong
-
+    def boxesInput(self, boxList, currentTime):
         # add all bounding boxes to plates if plates is empty
         if len(self.plates) == 0:
             for i in range(len(boxList)):
@@ -67,38 +71,33 @@ class objectlog:
                     return -1
 
                 newPlate = ArmorPlate(boxList[i], self.idAssign)
-
-                #commented out for syntax checking COMMENT BACK IN FOR ACTUAL THING
-                #***************************************************************
-                kinematic_Update(newPlate.getPosition[0], newPlate.getPosition[1], newPlate.getPosition[2])
-                kinematic_predict(currentTime - timestamp)
-                newPlate.updateVA(getVA())
-                #***************************************************************
+                newPlate.addArmorPlate(newPlate, currentTime)
 
                 self.plates.append(newPlate)
                 self.idAssign += 1 
         else:
-            for i in range(len(boxList)):
+            for box in boxList:
                 #check size of bounding box, if too small pass this iteration
-                if not self.size_check(boxList[i]):
+                if not self.size_check(box):
                     continue 
 
                 #error check the bounding box parameters
-                if((boxList[i].get_x_value() < 0) or (boxList[i].get_y_value() < 0) or (boxList[i].get_depth() < 0) or (boxList[i].get_height() < 0) or (boxList[i].get_width() < 0)):
+                if((box.get_x_value() < 0) or (box.get_y_value() < 0) or (box.get_depth() < 0) or (box.get_height() < 0) or (box.get_width() < 0)):
                     #bounding box is invalid
                     return -1
-
                 
-                new_armor = ArmorPlate(boxList[i], self.idAssign)
-                new_armor.predictPosition(currentTime)
+                # new_armor = ArmorPlate(boxList[i], self.idAssign)
+                # new_armor.predictPosition(currentTime)
 
                 # greedy stuff done here \/
-                assoc = self.assign_plate(new_armor, self.plates) #index of matching plate
+                assoc = self.assign_plate(box, self.plates) #index of matching plate
+                new_armor = ArmorPlate(box, self.idAssign)
                 if assoc == -1:
                     #new plate, not seen before
                     if len(self.plates) < 9:
                         # add new plate and we have space
                         self.plates.append(new_armor)
+                        self.idAssign += 1
                     else:
                         #looking at more than 9 things
                         print("need space")
@@ -108,72 +107,59 @@ class objectlog:
                     print("panic")
 
                 elif assoc == -3:
-                    #out of range in x,y, or z
+                    #out of range in x, y, or z (depth)
                     print("out of range")
                     
                 else:
-                    # associate new plate with plates[assoc]
                     # add new plate to list of associated plates of plates[assoc]
                     assoc_plate = self.plates[assoc]
-                    assoc_plate.addArmorPlate(new_armor)
+                    assoc_plate.addArmorPlate(new_armor, currentTime)
                     assoc_plate.timeBuffer = 0
+                    self.idAssign += 1
 
             # bump up timer buffers and remove dead plates 
-            kill_threshold = -1 #need to replace with an actual val
             for i in range(len(self.plates)):
                 p = self.plates[i]
                 if p.timeBuffer != 0:
                     # hasn't been seen, update count
                     p.timeBuffer += 1
                     if p.timeBuffer == kill_threshold:
-                        #kill plate
                         self.kill_plate(i)
             return
 
     def size_check(self, box) -> bool:
-        #10 is an abitrary num
         if box.get_height() * box.get_width() < MIN_AREA:
             return False
         return True
             
-        
-    # Input from prediction/errorchecking?
-    # unsure
-    # def predictionInput(self, input):
-    #     for i in input:
-    #         self.plates[i].updateVelocity(input[i].velocity)
-    #     return
-
-    # pseudo greedy (?)
-    # newPlate is the new armor plate we are trying to associate
-    def assign_plate(self, newPlate, plates) -> int: 
-        if newPlate == None or plates == None:
+    def assign_plate(self, box, plates) -> int: 
+        if box == None or plates == None:
             #error check
             return -2
-        predicted = newPlate.getNextPosition()
+        
+        position = box.get_position()
         #This variable accounts for the plate's position being the center of the bounding box
         #maybe factor in the plate boundaries into the out of range calculations
         #plate_radius = newPlate.getBoundingBox().get_width() / 2
         shortest_dist = float('inf')
-        shortest_plate = -1 # keeping the index, not decided what to do with the closest plate
+        shortest_plate = -1 # keeping the index of closest plate
 
-        if(((predicted[0] + margin_of_err) > MAX_X) or ((predicted[1] + margin_of_err) > MAX_Y) or ((predicted[2] + margin_of_err) > MAX_Z)
-           or ((predicted[0] - margin_of_err) < MIN_X) or ((predicted[1] - margin_of_err) < MIN_Y) or ((predicted[2] - margin_of_err) < MIN_Z)):       
+        if(((position[0] + margin_of_err) > MAX_X) or ((position[1] + margin_of_err) > MAX_Y) or ((position[2] + margin_of_err) > MAX_Z)
+           or ((position[0] - margin_of_err) < MIN_X) or ((position[1] - margin_of_err) < MIN_Y) or ((position[2] - margin_of_err) < MIN_Z)):       
             #if it is out of range, return early
             return -3
 
         for i in range(len(plates)):
-            dist = self.get_distance(self, newPlate, plates[i])
+            dist = self.get_distance(position, plates[i].getPosition())
             if dist < shortest_dist:
                 shortest_plate = i
                 shortest_dist = dist
 
         if shortest_dist > margin_of_err:
-            # need to assign a new plate
+            # knew plate, not seen before
             return -1
         return shortest_plate # this returns an index, but this could change to 
     # an actual refernce to the plate ¯\_(ツ)_/¯
-
 
     """
     Get_Distance returns the distance in pixels between two points
@@ -182,23 +168,27 @@ class objectlog:
     """
     def get_distance(self, point_one, point_two)-> float:
         
-        return np.sqrt(np.pow((point_one[0]-point_two[0]),2)\
-            +np.pow((point_one[1]-point_two[1]),2)\
-            +np.pow((point_one[2]-point_two[2]),2))
+        return np.sqrt( pow((point_one[0]-point_two[0]),2)\
+            + pow((point_one[1]-point_two[1]),2)\
+            + pow((point_one[2]-point_two[2]),2))
     
     #log all of the armor plates at the very end of the program before all the plates are deleted (genocide D:)
-    def kill_all(self, index):
+    def kill_all(self):
         for plate in self.plates:
             plate.writeToHistory(self.objectLogOutput)
-            self.kill_plate(self.plates.index(plate))
+        self.plates = []
         self.objectLogOutput.close()
         return
     
+    #used for debugging 
     def get_plates(self):
-        #used for debugging 
         return self.plates
 
     #logs and removes a single plate
-    def kill_plate(self, index):
-        self.plates[index].writeToHistory(self.objectLogOutput)
-        self.plates.remove(index)
+    def kill_plate(self, id):
+        for i in range(len(self.plates)):
+            plate = self.plates[i]
+            if plate.getID() == id:
+                plate.writeToHistory(self.objectLogOutput)
+                self.plates.pop(i)
+                break
