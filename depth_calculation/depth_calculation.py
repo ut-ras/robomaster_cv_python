4da31728b5261e94854b8d06deff06f3d09f6b25
Paul J. Han <pauljhan@utexas.edu>
2023-04-08 02:08:11 -0500

    slight correction get_data()

--- depth_calculation/depth_calculation.py
+++ depth_calculation/depth_calculation.py
@@ -47,7 +47,7 @@ def get_color_image():
     try:
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
-        color_image = np.asanyarray(color_frame)
+        color_image = np.asanyarray(color_frame.get_data())
 
         return color_image
     except:
