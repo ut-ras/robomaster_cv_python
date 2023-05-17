64d179479e71e14374549f7acc3943bfd9de5c2b
Paul J. Han <pauljhan@utexas.edu>
2023-05-17 01:45:52 -0500

    object detection initialization completed

--- object_detection.py
+++ object_detection.py
@@ -1,5 +1,41 @@
 import onnxruntime as ort
 
-model_path = ''
-artifacts_dir = ''
+model_path = 'last_with_shapes.onnx'
+artifacts_dir = 'tidl_output'
+sess = None
+def initialize_object_detections():
+	session_options = ort.SessionOptions()
+	
+	print("Available execution providers: ", ort.get_available_providers())
+
+	runtime_options = {
+		"platform":"J7",
+		"version":"8.2",
+		"artifacts_folder":artifacts_dir
+	}
+
+	desired_eps = ['TIDLExecutionProvider','CPUExecutionProvider']
+	sess = ort.InferenceSession(
+		model_path,
+		providers = desired_eps,
+		provider_options=[runtime_options,{}],
+		sess_options=session_options
+	)
+
+	input_details, = sess.get_inputs()
+	batch_size, channel, height, width = input_details.shape
+	print(f"Input shape: {input_details.shape}")
+
+	assert isinstance(batch_size, str) or batch_size == 1
+	assert channel == 3
+
+	input_name = input_details.name
+	input_type = input_details.type
+
+	print(f'Input "{input_name}": {input_type}')
+
+#def run_object_detections(image, )
+
+
+
 
