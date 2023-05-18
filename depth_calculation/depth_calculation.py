54e5eded42b15ab9a4e0cb8d527eba1c975d6efe
pauljhan01 <pauljhan@utexas.edu>
2023-05-18 03:53:05 -0500

    reducing depth sensor resolution

--- depth_calculation/depth_calculation.py
+++ depth_calculation/depth_calculation.py
@@ -33,12 +33,12 @@ def initialize_real_sense():
         print("The demo requires Depth camera with Color sensor")
         exit(0)
 
-    config.enable_stream(rs.stream.depth, 960, 540, rs.format.z16, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
     if device_product_line == 'D435i':
-        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     else:
-        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
     # Start streaming
     pipeline.start(config)
