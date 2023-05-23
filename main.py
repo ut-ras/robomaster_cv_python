288a753057f91da0fc5636d3cefa01a17cbb51b8
Paul J. Han <pauljhan@utexas.edu>
2023-05-23 00:51:02 -0500

    integrated sending message through UART

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from depth_calculation import depth_calculation as dp
 from object_detection import object_detection as od
+from communication import communication as com
 import cv2
 import numpy as np
 #begin initialization
@@ -7,19 +8,35 @@ def run_forever():
 	dp.initialize_real_sense()
 	detector = od.object_detector()
 	detector.initialize_object_detections()
+	com.initialize_communication()
 	boundingbox_list = []
 	while True:
 		color_image, depth_image = dp.get_color_depth_image()
 		detector.run_object_detections(color_image, boundingbox_list)
 		dp.set_all_bounding_box_depth_values(depth_image, boundingbox_list)
-		print("x, y coordinate of center of armor plate: ", boundingbox_list[-1].get_x_center(), boundingbox_list[-1].get_y_center())
+
+		#debugging
+		print("x coordinate of center of armor plate: ", boundingbox_list[-1].get_x_center())
+		print("y coordinate of center of armor plate: ", boundingbox_list[-1].get_y_center())
 		print("Depth value from last detected armor plate: ", boundingbox_list[-1].get_depth())
+
+		#TODO
+		#Obtain velocity, acceleration values from Kalman filter
+		#Research how the fuck you use the Kalman filter API
 		x_center = boundingbox_list[-1].get_x_center()
 		y_center = boundingbox_list[-1].get_y_center()
 		depth_value = boundingbox_list[-1].get_depth()
-		
+
+		#debugging
+		assert(isinstance(x_center,np.float32))
+		assert(isinstance(y_center,np.float32))
+		assert(isinstance(depth_value,np.float32))
+
+		#send message to dev board through UART
+		com.send_turret_data(x_center,y_center,depth_value,np.float32(0),np.float32(0),np.float32(0),np.float32(0),np.float32(0),np.float32(0),True)
 		boundingbox_list.clear()
-		# Show images
+
+		# Show images, debugging
 		cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
 		cv2.imshow('RealSense', color_image)
 		cv2.waitKey(1)
