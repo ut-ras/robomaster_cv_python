1844de8af63fba4a6bc1b9192743bec2e81dce34
pauljhan01 <pauljhan@utexas.edu>
2023-06-01 20:18:30 -0500

    sending data through UART

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ def run_forever():
 		color_image, depth_image = dp.get_color_depth_image()
 		detector.run_object_detections(color_image, boundingbox_list)
 
+		if(len(boundingbox_list)==0):
+			com.send_turret_data(xPos = np.float32(0.0), yPos = np.float32(0.0), zPos = np.float32(0.0)
+			,xVel = np.float32(0.0),yVel = np.float32(0.0), zVel = np.float32(0.0),
+			xAcc = np.float32(0.0), yAcc = np.float32(0.0), zAcc = np.float32(0.0),
+			hasTarget=False)
+			boundingbox_list.clear()
+			pass
+
 		dp.set_all_bounding_box_depth_values(depth_image, boundingbox_list)
 
 		# for i in range(len(boundingbox_list)):
@@ -28,16 +36,18 @@ def run_forever():
 			boundingbox_list[i].set_x_coord(coords[0])
 			boundingbox_list[i].set_y_coord(coords[1])
 			boundingbox_list[i].set_z_coord(coords[2])
-			# print(boundingbox_list[i].get_x_coord())
-			# print(boundingbox_list[i].get_y_coord())
-			# print(boundingbox_list[i].get_z_coord())
 
+		com.send_turret_data(xPos = boundingbox_list[0].get_x_coord(), yPos = boundingbox_list[0].get_y_coord(), zPos = boundingbox_list[0].get_z_coord(),
+			xVel = np.float32(0.0),yVel = np.float32(0.0), zVel = np.float32(0.0),
+			xAcc = np.float32(0.0), yAcc = np.float32(0.0), zAcc = np.float32(0.0),
+			hasTarget=True)
+		
 		boundingbox_list.clear()
 
 		# Show images, calculate time elapsed, debugging
-		cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-		cv2.imshow('RealSense', color_image)
-		cv2.waitKey(1)
+		# cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+		# cv2.imshow('RealSense', color_image)
+		# cv2.waitKey(1)
 
 
 
