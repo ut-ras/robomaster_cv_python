cf5c9439e6d8afc38b7ca7c6cd9ed5397129b4a5
Paul J. Han <pauljhan@utexas.edu>
2023-06-14 22:22:37 -0500

    startup code written and tested

--- startup/cv_startup.sh
+++ startup/cv_startup.sh
@@ -2,6 +2,9 @@
 
 #copy service file to /etc/systemd/system
 cp cv_startup.service /etc/systemd/system
+#give all users read/write/execute permissions
 chmod 777 /etc/systemd/system/cv_startup.service
-systemctl daemon-reload
-systemctl enable cv_startup.service
+#reload all systemd daemons
+sudo systemctl daemon-reload
+#enable cv_startup.service in systemd
+sudo systemctl enable cv_startup.service
