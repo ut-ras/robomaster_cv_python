6410e76e8a012f8a480d94bf9200f984c3cff423
Has02 <hasifshaikh02@gmail.com>
2023-06-17 00:46:07 -0500

    Moved recording commands to main instead of depth_calculation

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from pixel_to_point import pixel_to_point as ptp
 import cv2
 import numpy as np
 import time
+
+#for saving images
+import os
+
+#Index number for frames for file saving
+frameIndex = 0
+
 #begin initialization
 def run_forever():
 	dp.initialize_real_sense()
@@ -17,26 +24,39 @@ def run_forever():
 	while True:
 		# start_time = time.time()
 		color_image, depth_image = dp.get_color_depth_image()
-		detector.run_object_detections(color_image, boundingbox_list)
+		#detector.run_object_detections(color_image, boundingbox_list)
 
-		if(len(boundingbox_list)==0):
-			com.send_turret_data(xPos = np.float32(0.0), yPos = np.float32(0.0), zPos = np.float32(0.0)
-			,xVel = np.float32(0.0),yVel = np.float32(0.0), zVel = np.float32(0.0),
-			xAcc = np.float32(0.0), yAcc = np.float32(0.0), zAcc = np.float32(0.0),
-			hasTarget=False)
-			boundingbox_list.clear()
-			continue
+		#save frames into file
+		frameIndex += 1
+		file_path = ""  #insert file path here if desired
+		file_name = "video"
 
-		dp.set_all_bounding_box_depth_values(depth_image, boundingbox_list)
 
-		ptp.set_point_coords(boundingbox_list,intrinsics)
+		cv2.imwrite(file_name + str(frameIndex) + ".png",color_image)
+        #below version includes saving it to a specific place in the file path
+        # cv2.imwrite(os.path.join(file_path,file_name + str(frameIndex)+ ".png"),color_image)
 
-		com.send_turret_data(xPos = boundingbox_list[0].get_x_coord(), yPos = boundingbox_list[0].get_y_coord(), zPos = boundingbox_list[0].get_z_coord(),
-			xVel = np.float32(0.0),yVel = np.float32(0.0), zVel = np.float32(0.0),
-			xAcc = np.float32(0.0), yAcc = np.float32(0.0), zAcc = np.float32(0.0),
-			hasTarget=True)
 		
-		boundingbox_list.clear()
+		# Commented out for sole purpose is to collect and save frame
+
+		# if(len(boundingbox_list)==0):
+		# 	com.send_turret_data(xPos = np.float32(0.0), yPos = np.float32(0.0), zPos = np.float32(0.0)
+		# 	,xVel = np.float32(0.0),yVel = np.float32(0.0), zVel = np.float32(0.0),
+		# 	xAcc = np.float32(0.0), yAcc = np.float32(0.0), zAcc = np.float32(0.0),
+		# 	hasTarget=False)
+		# 	boundingbox_list.clear()
+		# 	continue
+
+		# dp.set_all_bounding_box_depth_values(depth_image, boundingbox_list)
+
+		# ptp.set_point_coords(boundingbox_list,intrinsics)
+
+		# com.send_turret_data(xPos = boundingbox_list[0].get_x_coord(), yPos = boundingbox_list[0].get_y_coord(), zPos = boundingbox_list[0].get_z_coord(),
+		# 	xVel = np.float32(0.0),yVel = np.float32(0.0), zVel = np.float32(0.0),
+		# 	xAcc = np.float32(0.0), yAcc = np.float32(0.0), zAcc = np.float32(0.0),
+		# 	hasTarget=True)
+		
+		# boundingbox_list.clear()
 
 		# Show images, calculate time elapsed, debugging
 		# cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
